39b9558a4b2d6dd7a6eb31c1bbfdf0fabf0e24e1
ibne86 <khanibne@gmail.com>
2024-06-02 21:42:37 +0200

    adding menu elements and function for home page

--- cypress/pageObjects/homePage.js
+++ cypress/pageObjects/homePage.js
@@ -1,8 +1,8 @@
 class homePage {
     elements = {
       searchBox: () => cy.get("#suggestion-search"),
-      searchResult: () =>
-        cy.get(".searchResult__constTitle").contains("Nicolas Cage"),
+      searchResult: () => cy.get(".searchResult__constTitle").contains("Nicolas Cage"),
+      menuTab: () => cy.get(".ipc-responsive-button__text").contains("Menu"),
     };
   
     enterSearchText(query) {
@@ -12,6 +12,11 @@ class homePage {
     clickSerachResult() {
       this.elements.searchResult().click();
     }
+
+    expandMenuTab() {
+      this.elements.menuTab().click();
+    }
+
   }
   const home = new homePage();
   export default home;
