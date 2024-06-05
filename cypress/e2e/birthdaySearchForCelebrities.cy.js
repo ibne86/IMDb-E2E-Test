578cd934afbb6cdcc49a99b4c5a5f61d056a05f5
ibne86 <khanibne@gmail.com>
2024-06-05 21:22:46 +0200

    adding test script for searching celebrity with yesterday's date

---
+++ cypress/e2e/birthdaySearchForCelebrities.cy.js
@@ -0,0 +1,22 @@
+import menu from "../pageObjects/menuPage";
+import searchResult from "../pageObjects/searchResultPage";
+import thirdCelebrity from "../pageObjects/thirdCelebrityPage";
+import { getYesterdaysDate } from "../support/dateUtils";
+
+describe('Birthday SearchFor Celebrities', () => {
+  it('Search for celebrities born yesterday and take screechot of the third celebritie', () => {
+    cy.visitWebsite();
+    cy.expandMenuTab();
+    menu.click_bornToday();
+    searchResult.delete_deafultSeachButton();
+    searchResult.expand_birthdayTab();
+    searchResult.click_birthdayInputField();
+
+
+    const yesterdaysDate = getYesterdaysDate();
+    searchResult.set_birthdayInputField(yesterdaysDate);
+    searchResult.click_searchResultButton();
+    searchResult.click_thirdcelebrity();
+    thirdCelebrity.take_screenshot();
+  });
+});
