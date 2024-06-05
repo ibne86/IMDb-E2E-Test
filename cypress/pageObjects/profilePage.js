4973c8bff2f6b1c81c3bf87f4d12c95012b69293
ibne86 <khanibne@gmail.com>
2024-06-05 10:11:35 +0200

    adding locator and function to verify movie title

--- cypress/pageObjects/profilePage.js
+++ cypress/pageObjects/profilePage.js
@@ -1,22 +1,21 @@
 class profilePage {
-    elements = {
-      upComingTab: () => cy.get('label[data-testid="accordion-item-actor-upcoming-projects"] li').eq(0),
-      movieListLonglegs: () => cy.get('[data-testid="accordion-item-content-container"] ul li').contains("Longlegs"),
+  elements = {
+    upComingTab: () => cy.get('label[data-testid="accordion-item-actor-upcoming-projects"] li').eq(0),
+    movieListLonglegs: () => cy.get('[data-testid="accordion-item-content-container"] ul li').contains("Longlegs"),
+    movieTitle: () => cy.get('span[data-testid="hero__primary-text"]'),
+  };
 
-    };
-  
-    click_upcomingTab() {
-      this.elements.upComingTab().click();
-    }
-
-    click_FirstCompletedMovie() {
-      this.elements.movieListLonglegs().click();
-    }
-
-    verify_MovieTitle() {
-    this.elements.movieTitle();
-    }
+  click_upcomingTab() {
+    this.elements.upComingTab().click();
   }
-  const profile = new profilePage();
-  export default profile;
-  
+
+  click_FirstCompletedMovie() {
+    this.elements.movieListLonglegs().click();
+  }
+
+  verify_movieTitle(expectedTitle) {
+  this.elements.movieTitle().should('have.text', expectedTitle);
+  }
+}
+const profile = new profilePage();
+export default profile;
