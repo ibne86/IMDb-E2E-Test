d32e05ec3b897c0580d85c56763a1cb845c37654
ibne86 <khanibne@gmail.com>
2024-06-06 12:21:29 +0200

    adding test script to find celecrites born 40 years from current date

---
+++ cypress/e2e/celebritiesBornFourtyYearsAgo.cy.js
@@ -0,0 +1,24 @@
+import menu from "../pageObjects/menuPage";
+import searchResult from "../pageObjects/searchResultPage";
+import { getFoutryYearDate } from "../support/dateUtils";
+import { getCurrentDate } from "../support/dateUtils";
+
+describe("search for celebrities born exactly 40 years ago", () => {
+  it("Search for celebrities born born exactly 40 years ago and take screechot", () => {
+    cy.visitWebsite();
+    cy.expandMenuTab();
+    menu.click_bornToday();
+    searchResult.delete_deafultSeachButton();
+    searchResult.expand_birthDateTab();
+
+    const pastDate = getFoutryYearDate();
+    searchResult.set_fromBirthDatePicker(pastDate);
+
+    const currentDate = getCurrentDate();
+    searchResult.set_toBrithDateInputField(currentDate);
+    searchResult.click_searchResultButton();
+
+    searchResult.checkLink_firstCelebrityDescription();
+    searchResult.take_screenshot();
+  });
+});
