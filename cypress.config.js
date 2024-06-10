7622a61d4d5ca7d2ab037672f47fee13f09cb22d
ibne86 <khanibne@gmail.com>
2024-06-10 02:52:18 +0200

    Configure the visual regression plugin and environment variables

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,14 @@
 const { defineConfig } = require('cypress');
+const { configureVisualRegression } = require('cypress-visual-regression');
 
 module.exports = defineConfig({
   e2e: {
+    env: {
+      visualRegressionType: 'base'
+    },
+    screenshotsFolder: './cypress/snapshots/actual',
     setupNodeEvents(on, config) {
+      configureVisualRegression(on)
       on('before:browser:launch', (browser = {}, launchOptions) => {
         if (browser.name === 'chrome' && browser.isHeadless) {
           launchOptions.args.push('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36');
